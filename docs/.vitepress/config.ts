70471630383ab5b285c55ae4bcf4e04199b1dde8
curder <q.curder@gmail.com>
2022-12-27 16:18:45 +0800

    Add prev and next translate

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -12,6 +12,10 @@ export default defineConfig({
         outlineTitle: "章节导航",
         outline: 'deep',
         lastUpdatedText: "最后更新时间",
+        docFooter: {
+            prev: '上一页',
+            next: '下一页'
+        },
         editLink: {
             pattern: "https://github.com/curder/alpine.js-study/edit/master/docs/:path",
             text: '编辑它'
