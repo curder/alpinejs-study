91e92d0fc681262fc6257ca04131d7581b4d2dc9
curder <q.curder@gmail.com>
2022-12-29 20:57:57 +0800

    Add navigation children group

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -44,8 +44,14 @@ function nav() {
         {text: '指令', link: "/guide/directives/index", activeMatch: "/guide/directives/"},
         {text: "魔术属性", link: "/guide/magics/index", activeMatch: "/guide/magics/"},
         {text: "全局变量", link: "/guide/globals/index", activeMatch: "/guide/globals/"},
-        {text: '组件', link: '/guide/components/index', activeMatch: '/guide/components/'},
-        {text: "插件", link: "/guide/plugins/index", activeMatch: "/guide/plugins/"},
+        {
+            text: "其他",
+            activeMatch: "guide/globals|components|plugins",
+            items: [
+                {text: '组件', link: '/guide/components/index', activeMatch: '/guide/components/'},
+                {text: "插件", link: "/guide/plugins/index", activeMatch: "/guide/plugins/"},
+            ]
+        },
     ];
 }
 
