f0314b5bfcbeaf2d755782fedb981d9c24d4fbf1
curder <curder@users.noreply.github.com>
2022-12-31 04:25:57 +0000

    deploy: 46b168c88cad946be3095f7226279b2efbbd945b

---
+++ assets/guide_magics_refs.md.1d4d096d.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,o as n,a as l}from"./app.71d39663.js";const u=JSON.parse('{"title":"$refs","description":"","frontmatter":{},"headers":[],"relativePath":"guide/magics/refs.md","lastUpdated":1672304815000}'),o={name:"guide/magics/refs.md"},p=l("",7),e=[p];function t(c,r,D,F,y,i){return n(),a("div",null,e)}const C=s(o,[["render",t]]);export{u as __pageData,C as default};
