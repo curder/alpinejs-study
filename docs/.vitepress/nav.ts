c82110666747fc610670aec7e38b19d71b430606
curder <q.curder@gmail.com>
2024-07-24 13:45:17 +0800

    feat: Remove unused "others" section from nav

--- docs/.vitepress/nav.ts
+++ docs/.vitepress/nav.ts
@@ -5,14 +5,7 @@ const nav: DefaultTheme.NavItem[] = [
     {text: '指令', link: "/guide/directives/index", activeMatch: "/guide/directives/"},
     {text: "魔术属性", link: "/guide/magics/index", activeMatch: "/guide/magics/"},
     {text: "全局变量", link: "/guide/globals/index", activeMatch: "/guide/globals/"},
-    {
-        text: "其他",
-        activeMatch: "guide/components|plugins/",
-        items: [
-            // {text: '组件', link: '/guide/components/index', activeMatch: '/guide/components/'},
-            {text: "插件", link: "/guide/plugins/index", activeMatch: "/guide/plugins/"},
-        ]
-    },
+    {text: "插件", link: "/guide/plugins/index", activeMatch: "/guide/plugins/"},
 ];
 
 export default nav
